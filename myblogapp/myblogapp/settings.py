6666915425427a53720f1ec0ee0cd8fef07f4c88
seran72 <sdiriba33@gmail.com>
2024-04-13 16:51:31 -0700

    deploy to vercel

--- myblogapp/myblogapp/settings.py
+++ myblogapp/myblogapp/settings.py
@@ -24,8 +24,8 @@ SECRET_KEY = 'django-insecure-kgz0rop2hb4dve55r7*bof@qs+63=@g)!egosivo94&mejf+w7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".vercel.app"]
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+# ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myblogapp',
     'posts',
     'users'
 ]
